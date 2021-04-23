d51df1d74b3f99851fb49bfae4be05d246fcfaf7
chenmin <1002284135@qq.com>
2021-04-23 17:10:20 +0800

    隐藏appkey

--- src/Api.php
+++ src/Api.php
@@ -28,7 +28,7 @@ class Api extends AbstractAPI
     public function request($method, $url, $options = [])
     {
         $options['headers']['appid'] = $this->appId;
-        $options['headers']['appkey'] = $this->secret;
+        //$options['headers']['appkey'] = $this->secret;
         $timestamp = time();
         $random = str_pad(mt_rand(0, 999999), 6, "0", STR_PAD_LEFT);
         $sign=md5($this->appId.$this->secret.$timestamp.$random);
